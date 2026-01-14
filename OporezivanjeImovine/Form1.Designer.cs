6e100a58f3a078ae24708f6d71b6740312295614
Jopa <Jopa@DESKTOP-VUD2E6L>
2026-01-14 15:57:18 +0100

    Add project files.

---
+++ OporezivanjeImovine/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace OporezivanjeImovine
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbl_mail = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.btn_Uloguj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_login
+            // 
+            this.lbl_login.AutoSize = true;
+            this.lbl_login.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_login.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_login.Location = new System.Drawing.Point(396, 101);
+            this.lbl_login.Name = "lbl_login";
+            this.lbl_login.Size = new System.Drawing.Size(79, 31);
+            this.lbl_login.TabIndex = 0;
+            this.lbl_login.Text = "Login";
+            this.lbl_login.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(465, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(465, 258);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lbl_mail
+            // 
+            this.lbl_mail.AutoSize = true;
+            this.lbl_mail.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_mail.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_mail.Location = new System.Drawing.Point(337, 194);
+            this.lbl_mail.Name = "lbl_mail";
+            this.lbl_mail.Size = new System.Drawing.Size(87, 31);
+            this.lbl_mail.TabIndex = 0;
+            this.lbl_mail.Text = "E-mail";
+            this.lbl_mail.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_password.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.Location = new System.Drawing.Point(337, 258);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(103, 31);
+            this.lbl_password.TabIndex = 0;
+            this.lbl_password.Text = "Lozinka";
+            this.lbl_password.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_Uloguj
+            // 
+            this.btn_Uloguj.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Uloguj.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Uloguj.Location = new System.Drawing.Point(377, 344);
+            this.btn_Uloguj.Name = "btn_Uloguj";
+            this.btn_Uloguj.Size = new System.Drawing.Size(146, 51);
+            this.btn_Uloguj.TabIndex = 2;
+            this.btn_Uloguj.Text = "Uloguj se";
+            this.btn_Uloguj.UseVisualStyleBackColor = false;
+            // 
+            // Form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::OporezivanjeImovine.Properties.Resources.pozadina;
+            this.ClientSize = new System.Drawing.Size(948, 596);
+            this.Controls.Add(this.btn_Uloguj);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_mail);
+            this.Controls.Add(this.lbl_login);
+            this.Name = "Form_Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_login;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbl_mail;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Button btn_Uloguj;
+    }
+}
+
